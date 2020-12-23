3ad1a90fdaf26feb2956c3fa06a5ab7bbcfb58bd
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2020-12-23 18:26:59 -0500

    Testing with more concurrency

--- .github/workflows/_generate.js
+++ .github/workflows/_generate.js
@@ -12,53 +12,73 @@ const nodeVersions = [
 ];
 
 const packages = [
-	['1', [
-		'air-sdk-1.0.7.4880-mac-zip',
+	['1-windows', [
 		'air-sdk-1.0.7.4880-windows',
-		'air-sdk-1.1.0.5790-mac-zip',
 		'air-sdk-1.1.0.5790-windows',
-		'air-sdk-1.5.0.7220-mac-zip',
 		'air-sdk-1.5.0.7220-windows',
-		'air-sdk-1.5.3.9120-mac-zip',
 		'air-sdk-1.5.3.9120-windows'
 	]],
-	['2', [
-		'air-sdk-2.0.1.12090-mac-zip',
+	['1-mac', [
+		'air-sdk-1.0.7.4880-mac-zip',
+		'air-sdk-1.1.0.5790-mac-zip',
+		'air-sdk-1.5.0.7220-mac-zip',
+		'air-sdk-1.5.3.9120-mac-zip'
+	]],
+	['2-windows', [
 		'air-sdk-2.0.1.12090-windows',
-		'air-sdk-2.7.1.19610-mac-zip',
 		'air-sdk-2.7.1.19610-windows'
 	]],
-	['3.0', [
-		'air-sdk-3.0.0.4080-mac-zip',
+	['2-mac', [
+		'air-sdk-2.0.1.12090-mac-zip',
+		'air-sdk-2.7.1.19610-mac-zip'
+	]],
+	['3.0-windows', [
 		'air-sdk-3.0.0.4080-windows'
 	]],
-	['3.2', [
-		'air-sdk-3.2.0.2070-mac-zip',
+	['3.0-mac', [
+		'air-sdk-3.0.0.4080-mac-zip'
+	]],
+	['3.2-windows', [
 		'air-sdk-3.2.0.2070-windows'
 	]],
-	['3.6', [
-		'air-sdk-3.6.0.6090-mac-zip',
+	['3.2-mac', [
+		'air-sdk-3.2.0.2070-mac-zip'
+	]],
+	['3.6-windows', [
 		'air-sdk-3.6.0.6090-windows'
 	]],
-	['25', [
-		'air-sdk-25.0.0.134-mac-zip',
+	['3.6-mac', [
+		'air-sdk-3.6.0.6090-mac-zip'
+	]],
+	['25-windows', [
 		'air-sdk-25.0.0.134-windows'
 	]],
-	['27', [
-		'air-sdk-27.0.0.128-mac-zip',
+	['25-mac', [
+		'air-sdk-25.0.0.134-mac-zip'
+	]],
+	['27-windows', [
 		'air-sdk-27.0.0.132-windows'
 	]],
-	['32', [
-		'air-sdk-32.0.0.116-mac-zip',
+	['27-mac', [
+		'air-sdk-27.0.0.128-mac-zip'
+	]],
+	['32-windows', [
 		'air-sdk-32.0.0.116-windows'
 	]],
-	['32.0.2', [
-		'air-sdk-33.0.2.330-mac',
+	['32-mac', [
+		'air-sdk-32.0.0.116-mac-zip'
+	]],
+	['32.0.2-windows', [
 		'air-sdk-33.0.2.330-windows'
 	]],
-	['33', [
-		'air-sdk-33.1.1.345-mac',
+	['32.0.2-mac', [
+		'air-sdk-33.0.2.330-mac'
+	]],
+	['33-windows', [
 		'air-sdk-33.1.1.345-windows'
+	]],
+	['33-mac', [
+		'air-sdk-33.1.1.345-mac'
 	]]
 ];
 
