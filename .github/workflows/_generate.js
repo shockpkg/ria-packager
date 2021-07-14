59c0e5aca7de75a607145581c29e6e3f71064cbc
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2021-07-14 00:14:07 -0400

    Testing 33.1.1.554

--- .github/workflows/_generate.js
+++ .github/workflows/_generate.js
@@ -75,10 +75,10 @@ const packages = [
 		'air-sdk-33.0.2.330-mac'
 	]],
 	['33-windows', [
-		'air-sdk-33.1.1.533-windows'
+		'air-sdk-33.1.1.554-windows'
 	]],
 	['33-mac', [
-		'air-sdk-33.1.1.533-mac'
+		'air-sdk-33.1.1.554-mac'
 	]]
 ];
 
