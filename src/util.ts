c2f6fce69952d483fbbd9d88357e5e1bbbfd27de
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2023-10-07 03:29:15 -0400

    Legacy utils

--- src/util.ts
+++ src/util.ts
@@ -2,53 +2,6 @@
 import {quoteForCmd, quoteForSh} from 'puka';
 
 /**
- * Default value if value is undefined.
- *
- * @param value Value.
- * @param defaultValue Default value.
- * @returns Value or the default value if undefined.
- */
-export function defaultValue<T, U>(
-	value: T,
-	defaultValue: U
-): Exclude<T, undefined> | U {
-	// eslint-disable-next-line no-undefined
-	return value === undefined
-		? defaultValue
-		: (value as Exclude<T, undefined>);
-}
-
-/**
- * Default null if value is undefined.
- *
- * @param value Value.
- * @returns Value or null if undefined.
- */
-export function defaultNull<T>(value: T) {
-	return defaultValue(value, null);
-}
-
-/**
- * Default false if value is undefined.
- *
- * @param value Value.
- * @returns Value or false if undefined.
- */
-export function defaultFalse<T>(value: T) {
-	return defaultValue(value, false);
-}
-
-/**
- * Default true if value is undefined.
- *
- * @param value Value.
- * @returns Value or true if undefined.
- */
-export function defaultTrue<T>(value: T) {
-	return defaultValue(value, true);
-}
-
-/**
  * Trim dot flash from head of path.
  *
  * @param path Path string.
